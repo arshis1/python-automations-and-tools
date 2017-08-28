37b978090ebed4d3f5e0c8b8c50f0c6eca72e8c1
ArshiyaShaikh92 <arshiyashaikh92@gmail.com>
2017-08-29 01:06:52 +0530

    Assignment2

---
+++ Assignment2/fibonacci.py
@@ -0,0 +1,12 @@
+#Program to caluclate Fibonnaci series.
+#def fib():
+
+	
+def main():
+	count = 0
+	print("Enter the number of outcomes")
+	count = int(input())
+	print(count)
+
+if __name__=='__name__':
+	main()
