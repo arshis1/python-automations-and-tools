db547aad9009bbb2cb78ba9b3a88e3187c01bc9c
ArshiyaShaikh92 <arshiyashaikh92@gmail.com>
2017-09-02 19:29:59 +0530

    2nd sept
    2nd sept

--- Assignment2/fibonacci.py
+++ Assignment2/fibonacci.py
@@ -1,12 +1,19 @@
 #Program to caluclate Fibonnaci series.
-#def fib():
-
+class Fibonnaci:
+	def fib(n):
+		if (n==0 or n==1):
+			return n 
+		else:
+			return Fibonnaci.fib(n-1)+ Fibonnaci.fib(n-2)
 	
-def main():
-	count = 0
-	print("Enter the number of outcomes")
-	count = int(input())
-	print(count)
-
-if __name__=='__name__':
-	main()
+	def main():
+		count = 0
+		print("Enter the number of outcomes")
+		count = int(input())
+		print("The", count, "th fibonacci number is :")
+		print(Fibonnaci.fib(count))
+		
+#calling main function in class fibonnaci		
+#if __name__=='__name__':
+print("Calling main")
+a = Fibonnaci.main()
