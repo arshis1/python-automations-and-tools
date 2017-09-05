31b05b43061d329c2fc6c5de2a2b5f3500263d95
Kirtan Shetty <kirtan.shetty@hpslabdesigns.com>
2017-09-05 23:10:52 +0530

    Code changes that worked for me.

--- Assignment2/fibonacci.py
+++ Assignment2/fibonacci.py
@@ -2,19 +2,40 @@
 class Fibonnaci:
 	def fib(n):
 		if (n==0 or n==1):
-			return n 
+			return n
 		else:
 			return Fibonnaci.fib(n-1)+ Fibonnaci.fib(n-2)
-	
+
 	def main(self):
 		count = 0
 		print("Enter the number of outcomes")
 		count = int(input())
 		print("The", count, "th fibonacci number is :")
 		print(self.fib(count))
-		
-#calling main function in class fibonnaci		
+
+#calling main function in class fibonnaci
 #if __name__=='__name__':
 print("Calling main")
 a = Fibonnaci
 a.main()
+
+# class Fibonnaci:
+#   def fib(self, n):
+#     if (n==0 or n==1):
+#       return n
+#     else:
+#       return self.fib(n-1) + self.fib(n-2)
+
+#   def main(self):
+#     count = 0
+#     print("Enter the number of outcomes")
+#     count = int(input())
+#     print("The", count, "th fibonacci number is :")
+#     print(self.fib(count))
+
+# #calling main function in class fibonnaci
+# #if __name__=='__name__':
+# print("Calling main")
+# # a = Fibonnaci.main()
+# a = Fibonnaci()
+# a.main()
