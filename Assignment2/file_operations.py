04d58fc9a09368fa013b0a8bf5f3ed1eeccc878a
ArshiyaShaikh92 <arshiyashaikh92@gmail.com>
2017-09-07 21:11:50 +0530

    Add files via upload
    This code will read the file README in the Python folder.
    Change the directory in the code to where your python.exe is placed

---
+++ Assignment2/file_operations.py
@@ -0,0 +1,45 @@
+#Program that will read a file and print it on the console.
+# class Read:
+# 
+#   def read_file():
+#     print("**********************\n")
+#     print("Now reading file\n")
+#     f = open(r'''C:\Python31\README.txt''')
+#     Read.print_file(f)
+#   
+#   def print_file(f):
+#     print("**********************\n")
+#     print("Display the file\n")
+#     print(f.read())
+#   
+#   def main():
+#     print("**************************\n")
+#     print("Inside main\n")
+#     Read.read_file()
+#      
+# print("Calling main\n")  
+# a = Read
+# a.main()
+
+
+class Read:
+
+  def read_file():
+    print("**********************\n")
+    print("Now reading file\n")
+    f = open(r'''C:\Python31\README.txt''')
+    a.print_file(f)
+  
+  def print_file(f):
+    print("**********************\n")
+    print("Display the file\n")
+    print(f.read())
+  
+  def main():
+    print("**************************\n")
+    print("Inside main\n")
+    a.read_file()
+     
+print("Calling main\n")  
+a = Read
+a.main()
