0a84810a12db59867bc1ae14254209562722acdf
Arshiya Shaikh <arshiyashaikh92@gmail.com>
2024-11-09 20:44:17 +0000

    binar search algo

--- linear_search_algorithm.py
+++ linear_search_algorithm.py
@@ -1,13 +1,17 @@
 def linear_search(lst,target):
-    for i in range(len(lst)):
+    n= len(lst)
+    for i in range(n):
         if lst[i] == target:
+            #print(lst[i])
             return i
-        else:
-            return -1
+        
+    return -1
+
+
 
 arr = [1,45,87,67,55,101]
 target = 45
-ta1= 3
+ta1= 1
 ta2 = 101
 
 print(linear_search(arr,target))
